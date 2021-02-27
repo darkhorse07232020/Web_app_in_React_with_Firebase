4a9908c03551199124504b8fae5fe939bbf6d076
Darkhorse <darkhorse0723@outlook.com>
2021-02-27 06:41:01 +0300

    Create Avatar with Badge in Sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Nav, Image, Container, Row, Col} from 'react-bootstrap'
 import LogoImage from '../assets/images/PavleuLogoYellow.png'
+import AvatarImage from '../assets/images/avatar.png'
 import '../assets/css/sidebar.css'
 
 function sidebar() {
@@ -8,13 +9,22 @@ function sidebar() {
         <Nav className="col-md-3 d-none d-md-block sidebar p-3 custom-sidebar"
             activeKey="/home"
             onSelect={selectedKey => alert(`selected ${selectedKey}`)}
-            >
+        >
             <div className="sidebar-sticky"></div>
             <Container>
                 <Image src={LogoImage} fluid width = "100px" height = "100px" />
             </Container>
             <Row>
-                <Col >
+                <Col md={3}>
+                    <div className="avatar">
+                        <Image src={AvatarImage} fluid roundedCircle className="bg-white"></Image>
+                        <div
+                            className="badge-for-avatar bg-success rounded-circle border border-white"
+                        ></div>
+                    </div>
+                </Col>
+                <Col md={9} className="align-self-center px-0">
+                    <h3 className="text-white"><b>Rachel Frank</b></h3>
                 </Col>
             </Row>
             <Nav.Item>
